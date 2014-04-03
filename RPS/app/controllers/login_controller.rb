660af88c6d227e0399b5cf5800bf9ded62d3da83
williams4027 <williams.4027@gmail.com>
2014-04-03 12:55:47 -0400

    Adding functionality for the Create User.

--- RPS/app/controllers/login_controller.rb
+++ RPS/app/controllers/login_controller.rb
@@ -1,4 +1,9 @@
 class LoginController < ApplicationController
   def index
   end
+  
+  def parseUser
+	comments_from_form = params[:username];
+	puts "Read User: #{comments_from_form}";
+  end
 end
--- RPS/config/routes.rb
+++ RPS/config/routes.rb
@@ -1,5 +1,7 @@
 RPS::Application.routes.draw do
-  get "login/index"
+  match 'login/index', :via => :get
+  match 'login/parseUser', :via => :post
+  #get "login/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +10,7 @@ RPS::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get "login/parseUser"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
