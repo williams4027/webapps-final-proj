837bd2078b2095937b9545431f14414e0370f7d8
pccardwell <patcard123@gmail.com>
2014-04-03 12:49:02 -0400

    adding database model

---
+++ RPS/app/models/games.rb
@@ -0,0 +1,73 @@
+class Games < ActiveRecord::Base
+
+  def makeGame
+     	game = games.new
+	game.move = //retrieve from html
+	game.game_creator = //retrieve from current user.id
+	game.created_at = Time.now
+	game.save		
+  end
+
+  def openGames
+	op = games.where(:open => true)
+  end
+
+  def arena
+	battle = games.where(:id => //get from arena html?//)
+	gCreator = users.where(:id => //get from arena html)
+        mov2 = //get from arena html//
+	gChallenger = users.where(:id => //currently logged in user)
+	if (mov2 == "rock")
+		if (battle.move == "paper")
+			//rock loses
+			gChallenger.loses = gChallenger.loses + 1
+			gCreator.wins = gCreator.wins + 1
+	
+		elsif (battle.move == "scissors")
+			//rock wins
+			gChallenger.wins = gChallenger.wins + 1
+			gCreator.loses = gCreator.loses + 1
+
+		else	
+			//tie game
+			gChallenger.ties = gChallenger.ties + 1
+			gCreator.ties = gCreator.ties + 1
+		end
+	elsif (mov2 == "paper")
+		if (battle.move == "rock")
+			//paper wins
+			gChallenger.wins = gChallenger.wins + 1
+			gCreator.loses = gCreator.loses + 1
+
+		elsif (battle.move == "scissors")
+			//paper loses
+			gChallenger.loses = gChallenger.loses + 1
+			gCreator.wins = gCreator.wins + 1
+
+		else	
+			//tie game
+			gChallenger.ties = gChallenger.ties + 1
+			gCreator.ties = gCreator.ties + 1
+		end
+	else
+		if (battle.move == "paper")
+			//scissors wins
+			gChallenger.wins = gChallenger.wins + 1
+			gCreator.loses = gCreator.loses + 1
+
+		elsif (battle.move == "rock")
+			//scissors loses
+			gChallenger.loses = gChallenger.loses + 1
+			gCreator.wins = gCreator.wins + 1
+
+		else	
+			//tie game
+			gChallenger.ties = gChallenger.ties + 1
+			gCreator.ties = gCreator.ties + 1
+ 		end
+	end
+	battle.open = false
+	battle.save
+	gChallenger.save
+	gCreator.save
+end
---
+++ RPS/app/models/user.rb~
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+   def signUp
+	u = users.new
+	u.user_name = //from signup page
+	u.password = //from signup page
+	u.wins = 0
+	u.loses = 0
+	u.create_at = Time.now
+	u.save
+   end
+
+   
+end
